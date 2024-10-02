2cd5f8f81118a753a98258bc6c4f77bb6c5b3655
HighSylvie <vishubissa.s@gmail.com>
2024-10-02 13:08:29 +0530

    changes

--- TestRoom/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/', (req, res) => {
-    res.send('Working get request for users')
-})
-
-module.exports = router;
